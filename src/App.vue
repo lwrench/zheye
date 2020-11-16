f383bb39994ba236c4f9a75b1be2ce18ff60932c
lwrench <1243115181@qq.com>
2020-11-16 23:17:05 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container">
+    <column-list :list="list"></column-list>
+  </div>
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+import ColumnList, { ColumnProps } from '@/components/ColumnList.vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+const testData: ColumnProps[] = [
+  {
+    id: 1,
+    title: 'test1',
+    avatar: 'https://static.vue-js.com/6280b990-ff19-11ea-85f6-6fac77c0c9b3.png',
+    description: 'test1'
+  },
+  {
+    id: 2,
+    title: 'test2',
+    avatar: 'https://static.vue-js.com/6280b990-ff19-11ea-85f6-6fac77c0c9b3.png',
+    description: 'test2'
+  }
+]
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    ColumnList
+  },
+  setup () {
+    return {
+      list: testData
+    }
+  }
+})
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ColumnList.vue
@@ -0,0 +1,33 @@
+<template>
+  <ul>
+    <li v-for="column in list" :key="column.id">
+      <img :alt="column.title" :src="column.avatar">
+      <p>{{ column.title }}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+
+<script lang='ts'>
+import { defineComponent, PropType } from 'vue'
+
+export interface ColumnProps {
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+
+export default defineComponent({
+  name: 'ColumnList',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      required: true
+    }
+  }
+})
+</script>
+
+<style>
+</style>
