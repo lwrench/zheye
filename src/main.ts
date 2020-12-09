29c970a84946e08e1b18874e8c8888e1ea3cbfef
thousand <1243115181@qq.com>
2020-12-09 20:09:45 +0800

    axios

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://apis.imooc.com/api/'
+axios.interceptors.request.use(config => {
+  config.params = { ...config.params, icode: 'A20B42A673BD0A08' }
+  if (config.data instanceof FormData) {
+    config.data.append('icode', 'A20B42A673BD0A08')
+  } else {
+    config.data = { ...config.data, icode: 'A20B42A673BD0A08' }
+  }
+  return config
+})
+
+axios.get('/columns').then((res) => {
+  console.log(res.data)
+})
 
 createApp(App).mount('#app')
